2cb438909460f5965c84577abd19ee6762d0e325
raspserver <rhz@gmx.de>
2026-03-09 13:22:32 +0100

    update

--- js/main.js
+++ js/main.js
@@ -35,6 +35,31 @@ document.addEventListener("DOMContentLoaded", () => {
 	
 	
 	
+	/* Magnetic CTA Button */
+	const magneticButtons = document.querySelectorAll(".cta-button");
+
+		magneticButtons.forEach(btn => {
+
+		  btn.addEventListener("mousemove", (e) => {
+
+			const rect = btn.getBoundingClientRect();
+
+			const x = e.clientX - rect.left - rect.width/2;
+			const y = e.clientY - rect.top - rect.height/2;
+
+			btn.style.transform = `translate(${x*0.18}px, ${y*0.18}px)`;
+
+		  });
+
+		  btn.addEventListener("mouseleave", () => {
+			btn.style.transform = "";
+		  });
+
+		});
+
+
+
+
 
 
 
