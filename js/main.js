2cb2b30587c8ffd236041259e7d35352f6a0e22f
raspserver <rhz@gmx.de>
2026-04-07 23:56:22 +0200

    update

--- js/main.js
+++ js/main.js
@@ -3755,7 +3755,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		/* Harte Untergrenze, weil dein Layout real mehr Platz braucht
 		   als die Berechnung zuverlässig liefert */
 		const absoluteMinExtra =
-			window.innerWidth <= 768 ? 500 : 600;
+			window.innerWidth <= 768 ? 480 : 600;
 
 		const extraHeight = Math.max(
 			absoluteMinExtra,
