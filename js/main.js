6cc7f1d7735f76642f52b6ffd4a5d436aebf6a78
raspserver <rhz@gmx.de>
2026-04-08 16:05:38 +0200

    update

--- js/main.js
+++ js/main.js
@@ -3525,7 +3525,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	},
 	
 	getHeroCalendarAutoCloseOffset() {
-		return window.innerWidth <= 768 ? 60 : 24;
+		return window.innerWidth <= 768 ? 160 : 24;
 	},
 
 	positionHeroCalendar() {
