72e4490e64b5b791b7743f98a16b39d612cbdf04
raspserver <rhz@gmx.de>
2026-04-09 22:57:55 +0200

    update

--- js/main.js
+++ js/main.js
@@ -4321,7 +4321,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			
 			center: salonCoords,
 			//~ zoom: 16.9,
-			zoom: 11.0,
+			zoom: 14.0,
 			pitch: 52,
 			bearing: -14,
 
@@ -4342,7 +4342,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			  this.map.easeTo({
 				center: salonCoords,
 				//~ zoom: 17.15,
-				zoom: 10.00,
+				zoom: 15.00,
 				pitch: 56,
 				bearing: -18,
 				duration: 2200,
