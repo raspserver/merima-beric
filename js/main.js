28b96ee8931a90c2ece8c71df906e4bf2b1d1fa1
raspserver <rhz@gmx.de>
2026-04-12 22:15:37 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+import { state } from "./core/state.js";
 import { scrollSectionHintModule } from "./modules/scrollSectionHintSystem/scrollSectionHintModule.js";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -283,86 +284,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // ---------------------------------------------------------------------
   // 5) ZENTRALER STATE
   // ---------------------------------------------------------------------
-  const state = {
-    lastScrollY: window.scrollY,
-    scrollVelocity: 0,
-    scrollDirection: "down",
-
-    animation: {
-      running: false,
-      lastFrameTime: performance.now(),
-    },
-
-    nav: {
-      visible: createAnimatedValue(0),
-      compact: createAnimatedValue(0),
-      surface: createAnimatedValue(0),
-      gestureStretch: createAnimatedValue(0),
-      manualOpen: false,
-    },
-
-    hero: {
-      parallax: createAnimatedValue(0),
-    },
-	
-	cta: {
-	  elasticY: createAnimatedValue(0),
-	},
-
-    scroll: {
-      programmatic: false,
-      mode: null,
-      activeAnimation: null,
-      activeToken: 0,
-      topSettleRaf: null,
-      topSettleTimeout: null,
-    },
-
-    touch: {
-      active: false,
-    },
-
-
-	ui: {
-		suppressCtaHoverCleanup: null,
-		suppressNextClick: false,
-		ctaDefaultLabel: "",
-		
-		fullCalendarInstance: null,
-		fullCalendarResizeTimer: null,
-		
-		heroCalendarOpen: false,
-		heroCalendarExtraHeight: 0,
-		heroCalendarAnimating: false,
-		heroCalendarLayoutRaf: null,
-		heroCalendarRevealTimer: null,
-		heroCalendarMeasuredTop: 0,
-		heroCalendarMeasuredHeight: 0,
-		heroCalendarMeasuredExtra: 0,
-		heroCalendarKeepCtaFlat: false,
-		heroCalendarNavbarFreeze: false,
-		heroCalendarAutoCloseTimer: null,
-		heroCalendarAutoCloseArmed: false,
-		heroCalendarLastScrollY: window.scrollY,
-		heroCalendarPrewarmed: false,
-		heroCalendarPrewarmObserver: null,
-		
-		pendingNavAfterHeroCalendarCloseToken: 0,
-		
-		contactMapPrewarmed: false,
-		contactMapPrewarmObserver: null,
-		contactMapLastOutsideZone: null,
-		contactMapReinitArmed: false,
-		contactMapReinitTimer: null,
-		contactMapLastReinitAt: 0,
-		contactMapAnimated: false,
-		contactMapNavCinematicRequested: false
-		
-	},
-
-    orderedSections: [],
-  };
-
+  
   // ---------------------------------------------------------------------
   // 6) PHYSIK-/MOTION-WERTE
   // ---------------------------------------------------------------------
