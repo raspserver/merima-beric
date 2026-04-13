d407433a465e72651f294abf2b2053cddc5edb93
raspserver <rhz@gmx.de>
2026-04-13 17:08:23 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { physics } from "./core/physics.js";
 import { scrollEngine } from "./core/scrollEngine.js";
+import { SETTINGS } from "./core/settings.js";
 import { springs } from "./core/springs.js";
 import { state } from "./core/state.js";
 import { navbarModule } from "./modules/navbarModule.js";
@@ -28,28 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // ---------------------------------------------------------------------
   // 1) GLOBALE EINSTELLUNGEN
   // ---------------------------------------------------------------------
-  const SETTINGS = {
-    breakpoints: {
-      mobileNav: 968,
-      mobilePhysics: 768,
-    },
 
-    thresholds: {
-      directionLock: 8,
-      inertia: Math.min(document.documentElement.clientHeight * 0.6, 600),
-      sectionNavClickDelay: 240,
-    },
-
-    gallery: {
-      videoFiles: [
-        "videos/snaptik_7204469200172190982_hd.mp4",
-        "videos/snaptik_7208965603661499654_hd.mp4",
-        "videos/snaptik_7211607331648441605_hd.mp4",
-        "videos/snaptik_7444629475364474145_hd.mp4",
-      ],
-      swipeThreshold: 80,
-    },
-  };
 
   // ---------------------------------------------------------------------
   // 2) DOM-REFERENZEN
