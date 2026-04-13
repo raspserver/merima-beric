beda57a8377a3f26583958031805401acd7d9c84
raspserver <rhz@gmx.de>
2026-04-13 11:33:03 +0200

    update

--- js/modules/scrollSectionHintModule.js
+++ js/modules/scrollSectionHintModule.js
@@ -1,9 +1,9 @@
 
-import { state } from "../core/state.js";
-import { cssVar } from "../utils/cssVar.js";
-import { utils } from "../utils/utils.js";
-import { clamp } from '../utils/helper.js';
-import { scrollEngine } from "../modules/scrollEngine.js";
+import { state } from "../../core/state.js";
+import { cssVar } from "../../utils/cssVar.js";
+import { utils } from "../../utils/utils.js";
+import { clamp } from '../../utils/helper.js';
+import { scrollEngine } from "./scrollEngine.js";
 
 
 
