b3784fdd7af33d52f3363264b1a1ae6f59724a98
raspserver <rhz@gmx.de>
2026-04-15 16:26:01 +0200

    update

--- js/core/state.js
+++ js/core/state.js
@@ -1,84 +1,81 @@
 // state.js
 
-//~ import { createAnimatedValue, resetAnimatedValue } from "../utils/helper.js";
 import { utils } from "../utils/utils.js";
 
 export const state = {
-    lastScrollY: window.scrollY,
-    scrollVelocity: 0,
-    scrollDirection: "down",
+  lastScrollY: window.scrollY,
+  scrollVelocity: 0,
+  scrollDirection: "down",
 
-    animation: {
-      running: false,
-      lastFrameTime: performance.now(),
-    },
+  animation: {
+    running: false,
+    lastFrameTime: performance.now(),
+  },
 
-    nav: {
-      visible: utils.createAnimatedValue(0),
-      compact: utils.createAnimatedValue(0),
-      surface: utils.createAnimatedValue(0),
-      gestureStretch: utils.createAnimatedValue(0),
-      manualOpen: false,
-    },
+  nav: {
+    visible: utils.createAnimatedValue(0),
+    compact: utils.createAnimatedValue(0),
+    surface: utils.createAnimatedValue(0),
+    gestureStretch: utils.createAnimatedValue(0),
+    manualOpen: false,
+  },
 
-    hero: {
-      parallax: utils.createAnimatedValue(0),
-    },
-	
-	cta: {
-	  elasticY: utils.createAnimatedValue(0),
-	},
+  hero: {
+    parallax: utils.createAnimatedValue(0),
+  },
 
-    scroll: {
-      programmatic: false,
-      mode: null,
-      activeAnimation: null,
-      activeToken: 0,
-      topSettleRaf: null,
-      topSettleTimeout: null,
-    },
+  cta: {
+    elasticY: utils.createAnimatedValue(0),
+  },
 
-    touch: {
-      active: false,
-    },
+  scroll: {
+    programmatic: false,
+    mode: null,
+    activeAnimation: null,
+    activeToken: 0,
+    topSettleRaf: null,
+    topSettleTimeout: null,
+  },
 
+  touch: {
+    active: false,
+  },
 
-	ui: {
-		suppressCtaHoverCleanup: null,
-		suppressNextClick: false,
-		ctaDefaultLabel: "",
-		
-		fullCalendarInstance: null,
-		fullCalendarResizeTimer: null,
-		
-		heroCalendarOpen: false,
-		heroCalendarExtraHeight: 0,
-		heroCalendarAnimating: false,
-		heroCalendarLayoutRaf: null,
-		heroCalendarRevealTimer: null,
-		heroCalendarMeasuredTop: 0,
-		heroCalendarMeasuredHeight: 0,
-		heroCalendarMeasuredExtra: 0,
-		heroCalendarKeepCtaFlat: false,
-		heroCalendarNavbarFreeze: false,
-		heroCalendarAutoCloseTimer: null,
-		heroCalendarAutoCloseArmed: false,
-		heroCalendarLastScrollY: window.scrollY,
-		heroCalendarPrewarmed: false,
-		heroCalendarPrewarmObserver: null,
-		
-		pendingNavAfterHeroCalendarCloseToken: 0,
-		
-		contactMapPrewarmed: false,
-		contactMapPrewarmObserver: null,
-		contactMapLastOutsideZone: null,
-		contactMapReinitArmed: false,
-		contactMapReinitTimer: null,
-		contactMapLastReinitAt: 0,
-		contactMapAnimated: false,
-		contactMapNavCinematicRequested: false
-		
-	},
+  ui: {
+    suppressCtaHoverCleanup: null,
+    suppressNextClick: false,
+    ctaDefaultLabel: "",
 
-    orderedSections: [],
+    fullCalendarInstance: null,
+    fullCalendarResizeTimer: null,
+
+    heroCalendarOpen: false,
+    heroCalendarExtraHeight: 0,
+    heroCalendarAnimating: false,
+    heroCalendarLayoutRaf: null,
+    heroCalendarRevealTimer: null,
+    heroCalendarMeasuredTop: 0,
+    heroCalendarMeasuredHeight: 0,
+    heroCalendarMeasuredExtra: 0,
+    heroCalendarKeepCtaFlat: false,
+    heroCalendarNavbarFreeze: false,
+    heroCalendarAutoCloseTimer: null,
+    heroCalendarAutoCloseArmed: false,
+    heroCalendarLastScrollY: window.scrollY,
+    heroCalendarPrewarmed: false,
+    heroCalendarPrewarmObserver: null,
+
+    pendingNavAfterHeroCalendarCloseToken: 0,
+
+    contactMapPrewarmed: false,
+    contactMapPrewarmObserver: null,
+    contactMapLastOutsideZone: null,
+    contactMapReinitArmed: false,
+    contactMapReinitTimer: null,
+    contactMapLastReinitAt: 0,
+    contactMapAnimated: false,
+    contactMapNavCinematicRequested: false,
+  },
+
+  orderedSections: [],
 };
