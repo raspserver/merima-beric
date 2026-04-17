d96707c6f9bb83750c44ada442c39ef3ffd33209
raspserver <rhz@gmx.de>
2026-04-17 21:51:19 +0200

    update

--- js/modules/navbarModule.js
+++ js/modules/navbarModule.js
@@ -192,7 +192,9 @@ export const navbarModule = {
 	  // 🧊 KALENDER FREEZE
 	  // ---------------------------
 
-	  if (state.ui.heroCalendarOpen || state.ui.heroCalendarAnimating) {
+	  //~ if (state.ui.heroCalendarOpen || state.ui.heroCalendarAnimating) {
+
+		if (state.ui.heroCalendarAnimating) {
 		state.lastScrollY = currentY;
 		return;
 	  }
