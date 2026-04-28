851241aa6cb82344fe8e67d4f3de82209b7e329e
raspserver <rhz@gmx.de>
2026-04-29 00:23:16 +0200

    update

--- js/modules/heroCalendarModule.js
+++ js/modules/heroCalendarModule.js
@@ -52,7 +52,7 @@ export const heroCalendarModule = {
 	  initialView: "dayGridMonth",
 
 	  //~ height: "100%",
-	  contentHeight: "auto",
+	  height: "auto",
 	  firstDay: 1,
 	  weekends: true,
 	  navLinks: false,
