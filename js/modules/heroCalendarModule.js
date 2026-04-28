1b210b5cf334340dfc97d1ae4d290b8e77fa5a73
raspserver <rhz@gmx.de>
2026-04-29 00:59:47 +0200

    update

--- js/modules/heroCalendarModule.js
+++ js/modules/heroCalendarModule.js
@@ -404,7 +404,7 @@ export const heroCalendarModule = {
     
     //~ const preferredCalendarHeight = this.getHeroCalendarPreferredHeight();
     
-    const preferredCalendarHeight = this.getHeroCalendarPreferredHeight() + 120;
+    const preferredCalendarHeight = this.getHeroCalendarPreferredHeight() + 72;
     
     const desiredBottomOffset = utils.cssVar.lengthPx("--hero-cta-gap-to-boundary", 90);
     const ctaHeight = this.cta.getBoundingClientRect().height || 64;
